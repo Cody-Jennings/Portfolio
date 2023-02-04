ef481e770e7a50eafd68eaf2f55f2ec21ffa4047
Cody Jennings <cojack91@gmail.com>
2023-02-04 11:35:52 -0500

    adding data model

---
+++ src/Components/data.js
@@ -0,0 +1,29 @@
+// create an array of objects with my projects data
+
+export const projects = [
+    {
+        title: 'Big Bang Theory Decision Maker',
+        subtitle: 'JavaScript, HTML, CSS',
+        description: 'While the title may suggest otherwise this is simply an extended version of Rock, Paper, Scissors, with two added elements of Lizard and Spock. This game can be played amongst many people especially when making a final decision. Can be used to settle where to eat, what movie to watch, what direction to go etc. This app has the user play against the computer or "Shelbot"',
+        image: require('./'),
+        link: 'https://cody-jennings.github.io/Project1-BigBangTheoryDecisionMaker/',
+        github: 'https://github.com/Cody-Jennings/Project1-BigBangTheoryDecisionMaker',
+    },
+    {
+        title: "Cody's Survival Shop",
+        subtitle: 'JavaScript, HTML, CSS',
+        description:  ,
+        image: 
+        link: 'https://codys-survival-shop.onrender.com/',
+        github: 'https://github.com/Cody-Jennings/Project2-StoreApp',
+    },
+    {
+        title: 'Kool Kid Slang Translator App',
+        subtitle: 'Full Stack MERN App',
+        description:  ,
+        image: ,
+        link:  ,
+        github:  ,
+    },
+
+]
