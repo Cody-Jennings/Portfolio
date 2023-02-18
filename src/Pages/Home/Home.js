d104a376875f2a395cc7d8029150d780b31d014a
Cody Jennings <cojack91@gmail.com>
2023-02-17 22:17:02 -0500

    added home container

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        
+            <div className='home'>
+                <div className='home-container'>
+                    <h1>Home</h1>
+                </div>
+            </div>
+        
     )
 }
 
