9d42781a1eb9aa8a7d14229665b2f7d8f0ae7130
Cody Jennings <cojack91@gmail.com>
2023-02-20 15:47:31 -0500

    added portfolio picture and some styling

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,13 +3,19 @@ import './Home.css'
 
 function Home() {
     return (
-        
-            <div className='home'>
-                <div className='home-container'>
-                    <h1>Home</h1>
+
+        <div className='home'>
+            <div className='home-container'>
+                <h1>Cody Jennings</h1>
+                <p>Full Stack Software Engineer</p>
+                <br>
+                </br>
+                <div className='about-img'>
+                    <img src="../../../portfolioPicture.jpeg" ></img>
                 </div>
             </div>
-        
+        </div>
+
     )
 }
 
